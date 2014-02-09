765a3482c30440f3b9f7bc18c814f4bb5cc0b755
webmilio <therealwebmilio@gmail.com>
2014-02-09 11:48:25 -0500

    Keeping the progress going.

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
 <a href="./preface.php">Pr&eacute;face</a> || 
 <a href="./part01.php">Premi&egrave;re Partie</a> || 
 <a href="./part02.php">Deuxi&egrave;me Partie</a> || 
-<a href="./part03.php">Troisi&egrave;me Partie</a>
+<a href="./part03.php">Troisi&egrave;me Partie</a> || 
+<a href="./conclusion.php">Conclusion</a>
 </center>
 </div>
 </footer>

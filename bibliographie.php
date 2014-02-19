e8f19197d6057fc737c36d9b3ed153274b0600e7
webmilio <therealwebmilio@gmail.com>
2014-02-18 20:32:09 -0800

    Commit #16 - + biblio - Ajout de <a></a>

--- bibliographie.php
+++ bibliographie.php
@@ -1,8 +1,13 @@
 <?php include 'header.php'; ?>
 <h2><center><?php printWN(); ?>: <u>Bibliographie</u></center></h2>
 <br />
+<p>Reste a terminer.</p>
 <ul>
 <li><a class="biblio" href="http://forum.tip.it/topic/261182-equivalent-of-spoiler-in-html/">http://forum.tip.it/topic/261182-equivalent-of-spoiler-in-html/</a></li>
+<li><a href="http://www.w3schools.com/">http://www.w3schools.com/</a><br />
+- <a href="http://www.w3schools.com/html/default.asp">http://www.w3schools.com/html/default.asp</a><br />
+- <a href="http://www.w3schools.com/html/default.asp">http://www.w3schools.com/css/default.asp</a><br />
+- <a href="http://www.w3schools.com/html/default.asp">http://www.w3schools.com/php/default.asp</a><br /></li>
 </ul>
 <br />
 <?php include "footer.php"; ?>
--- part02.php
+++ part02.php
@@ -3,6 +3,13 @@
 <h3 class="subtitle"><u>&lt;p&gt;</u></h3>
 <p>Si il y a bien une balise (autre que ceux de la <a href="./part01.php">premi&egrave;re partie</a>) qui est extr&egrave;mement simple &agrave; comprendre, c'est <code>&lt;p&gt;</code> et <code>&lt;/p&gt;</code>. En fait, ce que vous lisez fait parti de ces balises. Malgr&eacute; le fait que le texte sur une page fait majoritairement partie du corps de la page, elle peut se situer n'importe o&ugrave; entre les balises <code>&lt;html&gt;</code> et <code>&lt;/html&gt;</code>. Voiçi un exemple: <?php spoiler("<code>&lt;p&gt;Ceci est une ligne de texte.&lt;/p&gt;</code><br /><p>Ceci est une ligne de texte.</p>"); ?></p>
 
+<h3 class="subtitle"><u>&lt;a&gt;</u></h3>
+<p>La deuxi&egrave;me balise que je tiens &agrave; vous montrer. Cette balise sert &agrave; cr&eacute;er des liens et a plusieurs param&egrave;tres:
+<ul>
+<li><code>href</code></li>
+<li></li>
+</ul>
+
 <br /><br />
 <center><a href="./part01.php"><= Premi&egrave;re Partie</a> || <a href="./part03.php"> Troisi&egrave;me Partie =></a></center>
 <br /> 

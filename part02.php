20da7ae446681dce95fe3c1b81bb2a167021c8a9
webmilio <therealwebmilio@gmail.com>
2014-02-18 20:19:16 -0800

    Commit #15 - Ajout d'une exemple. - Gosh darn this .xml file is annoying.

--- part02.php
+++ part02.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <h2><center><?php printWN(); ?>: <u>Deuxi&egrave;me Partie</u></center></h2>
 <h3 class="subtitle"><u>&lt;p&gt;</u></h3>
-<p>Si il y a bien une balise (autre que ceux de la <a href="./part01.php">premi&egrave;re partie</a>) qui est extr&egrave;mement simple &agrave; comprendre, c'est <code>&lt;p&gt;</code> et <code>&lt;/p&gt;</code>. En fait, ce que vous lisez fait parti de ces balises. Malgr&eacute; le fait que le texte sur une page fait majoritairement partie du corps de la page, elle peut se situer n'importe o&ugrave; entre les balises <code>&lt;html&gt;</code> et <code>&lt;/html&gt;</code>.</p>
+<p>Si il y a bien une balise (autre que ceux de la <a href="./part01.php">premi&egrave;re partie</a>) qui est extr&egrave;mement simple &agrave; comprendre, c'est <code>&lt;p&gt;</code> et <code>&lt;/p&gt;</code>. En fait, ce que vous lisez fait parti de ces balises. Malgr&eacute; le fait que le texte sur une page fait majoritairement partie du corps de la page, elle peut se situer n'importe o&ugrave; entre les balises <code>&lt;html&gt;</code> et <code>&lt;/html&gt;</code>. Voiçi un exemple: <?php spoiler("<code>&lt;p&gt;Ceci est une ligne de texte.&lt;/p&gt;</code><br /><p>Ceci est une ligne de texte.</p>"); ?></p>
 
 <br /><br />
 <center><a href="./part01.php"><= Premi&egrave;re Partie</a> || <a href="./part03.php"> Troisi&egrave;me Partie =></a></center>

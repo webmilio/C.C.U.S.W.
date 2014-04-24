3325d85e58582305728f3adbb2ddb961571bb71b
webmilio <therealwebmilio@gmail.com>
2014-04-23 22:11:45 -0400

    Une erreur d'orthographe de moins :P

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <center>
 <p>Bienvenue sur mon site <b><?php printWN(); ?></b><br/>
 Sur ce site, je vous apprendrai comment programmer un site web de base mais pouvant servir à de multiple chose. Ce site comprend: une pr&eacute;face, 3 etapes principales, et finalement la conclusion.</p>
-    <a href="./preface.php">Pr&eacute;face</a>: outil(s) recommender.<br />
+    <a href="./preface.php">Pr&eacute;face</a>: outil(s) recommend&eacute;s.<br />
     <a href="./part01.php">Premi&egrave;re Partie</a>: balises de bases dans la cr&eacute;ation d'une page.<br />
     <a href="./part02.php">Deuxi&egrave;me Partie</a>: balises de base contenu d'une page.<br />
     <a href="./part03.php">Troisi&egrave;me Partie</a>: &agrave; venir.<br />
